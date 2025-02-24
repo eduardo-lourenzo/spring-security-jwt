34af417761268f16b3d960fc8c34c091a9df3815
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-24 17:51:42 -0300

    feat: create RoleEntity class

---
+++ src/main/java/br/edu/zup/spring_security_jwt/models/RoleEntity.java
@@ -0,0 +1,18 @@
+package br.edu.zup.spring_security_jwt.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleEnum name;
+}
--- src/main/java/br/edu/zup/spring_security_jwt/models/RoleEnum.java
+++ src/main/java/br/edu/zup/spring_security_jwt/models/RoleEnum.java
@@ -1,6 +1,6 @@
 package br.edu.zup.spring_security_jwt.models;
 
-public enum Role {
+public enum RoleEnum {
     ROLE_ADMIN,
     ROLE_USER;
 }
