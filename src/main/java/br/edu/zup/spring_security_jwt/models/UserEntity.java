ca6d6eb29ccab8a1f4cd298e90f3e41a6cbef670
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-24 18:14:20 -0300

    feat: create UserEntity class

---
+++ src/main/java/br/edu/zup/spring_security_jwt/models/UserEntity.java
@@ -0,0 +1,36 @@
+package br.edu.zup.spring_security_jwt.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<RoleEntity> roles;
+}
