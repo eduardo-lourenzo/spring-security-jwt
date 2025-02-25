85823205ada1dc753c8383f806dd140f1f37a26e
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-25 18:15:57 -0300

    feat: create TokenProviderJWT class

---
+++ src/main/java/br/edu/zup/spring_security_jwt/infra/jwt/TokenProviderJWT.java
@@ -0,0 +1,54 @@
+package br.edu.zup.spring_security_jwt.infra.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class TokenProviderJWT {
+    static final long FIVE_MIN_IN_MS = 300000; // 5min => 300s => 300.000ms
+    private final String secretKeyJWT = System.getenv("JWT_SECRETKEY");
+
+    public String generateToken(Authentication authentication) {
+        String userName = authentication.getName();
+        Date currentDate = new Date();
+        Date expirationDateInMilliseconds = new Date(currentDate.getTime() + FIVE_MIN_IN_MS);
+
+        return Jwts.builder()
+                .subject(userName)
+                .issuedAt(currentDate)
+                .expiration(expirationDateInMilliseconds)
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKeyJWT));
+    }
+
+    public String getUserName(String token) {
+
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+        return true;
+    }
+
+    // Refresh Token method
+}
