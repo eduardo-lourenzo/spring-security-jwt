044cec33c9e6b17eb07cabf466027c20ae4d10fc
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-25 18:47:37 -0300

    feat: create SecurityConfig class

---
+++ src/main/java/br/edu/zup/spring_security_jwt/infra/SecurityConfig.java
@@ -0,0 +1,61 @@
+package br.edu.zup.spring_security_jwt.infra;
+
+
+import br.edu.zup.spring_security_jwt.infra.jwt.AuthenticationEntryPointJWT;
+import br.edu.zup.spring_security_jwt.infra.jwt.AuthenticationFilterJWT;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+    private UserDetailsService userDetailsService;
+    private AuthenticationEntryPointJWT authenticationEntryPoint;
+    private AuthenticationFilterJWT authenticationFilter;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) ->
+                        {
+                            authorize.requestMatchers("/register").permitAll();
+                            authorize.requestMatchers("/login").permitAll();
+                            authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                            authorize.anyRequest().authenticated();
+                            // /"admin" and "/user" authenticated
+                        }
+                ).httpBasic(Customizer.withDefaults());
+
+        http.exceptionHandling(exception -> exception
+                .authenticationEntryPoint(authenticationEntryPoint));
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
