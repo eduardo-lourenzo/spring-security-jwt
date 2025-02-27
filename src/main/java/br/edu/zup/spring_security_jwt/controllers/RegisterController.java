8c30b1badf438b03e284c0cb24ebd9cb14681f10
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-27 14:45:53 -0300

    fix: change from Get to Post mapping annotation

--- src/main/java/br/edu/zup/spring_security_jwt/controllers/RegisterController.java
+++ src/main/java/br/edu/zup/spring_security_jwt/controllers/RegisterController.java
@@ -1,18 +1,11 @@
 package br.edu.zup.spring_security_jwt.controllers;
 
 import br.edu.zup.spring_security_jwt.dtos.RegisterUserDTO;
-import br.edu.zup.spring_security_jwt.services.AdminService;
 import br.edu.zup.spring_security_jwt.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/register")
@@ -20,8 +13,8 @@ public class RegisterController {
     @Autowired
     private AuthService authService;
 
-    @GetMapping
-    public ResponseEntity<String> getAdmin(@RequestBody RegisterUserDTO registerUserDTO) {
+    @PostMapping
+    public ResponseEntity<String> registerUser(@RequestBody RegisterUserDTO registerUserDTO) {
         if (authService.registerUser(registerUserDTO)) {
             return new ResponseEntity<>(
                     "Usuário cadastrado com sucesso!",
