e69b2187a972fbe33e5f4edb12bb4f5a75e24e44
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-27 14:42:49 -0300

    feat: add findByType RoleEnum

--- src/main/java/br/edu/zup/spring_security_jwt/repositories/RoleRepository.java
+++ src/main/java/br/edu/zup/spring_security_jwt/repositories/RoleRepository.java
@@ -1,7 +1,11 @@
 package br.edu.zup.spring_security_jwt.repositories;
 
 import br.edu.zup.spring_security_jwt.models.RoleEntity;
+import br.edu.zup.spring_security_jwt.models.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> findByType(RoleEnum type);
 }
